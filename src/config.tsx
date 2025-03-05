edff8c57e8c11c0948aee0534277eaa5812d2d7e
mayukh551 <may14ukh@gmail.com>
2025-03-05 20:30:10 +0530

    backend url updated

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,5 @@
 export const BACKEND_URL2 =
   "https://150a-2405-201-8002-78df-c165-d1b3-2d00-e47a.ngrok-free.app";
-export const BACKEND_URL = "http://ec2-16-171-181-37.eu-north-1.compute.amazonaws.com:3000";
+export const BACKEND_URL = "https://quantum-backend-hw57.onrender.com";
 // export const BACKEND_URL = "http://localhost:3000";
 export const CLOUDFRONT_URL = "https://d2bvviipd4yuuo.cloudfront.net";
